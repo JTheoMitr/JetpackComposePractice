bfa09c767d355b9a7268f65e0cdd0fffe249139a
John <jmitropoulos@alumni.berklee.edu>
2022-04-06 21:26:04 -0500

    Initial commit

---
+++ app/src/main/java/android/example/jetpackcomposepractice/Navigation.kt
@@ -0,0 +1,45 @@
+package android.example.jetpackcomposepractice
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Screen.MainScreen.route) {
+        composable(route = Screen.MainScreen.route) {
+
+        }
+    }
+}
+
+@Composable
+fun MainScreen(navController: NavController) {
+    var text by remember {
+        mutableStateOf("")
+    }
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 50.dp)
+    ) {
+        TextField(value = text, onValueChange = {
+            text = it
+        })
+    }
+}
---
+++ app/src/main/java/android/example/jetpackcomposepractice/Screen.kt
@@ -0,0 +1,6 @@
+package android.example.jetpackcomposepractice
+
+sealed class Screen(val route: String) {
+    object MainScreen : Screen("main_screen")
+    object DetailScreen : Screen("detail_screen")
+}
